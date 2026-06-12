72676742ff832b7594c8c0f85e48579f988200e1
bobosz44 <maksdobosz44@gmail.com>
2026-06-12 22:05:16 +0200

    dodanie kilku nowych klas

--- klasy.cpp
+++ klasy.cpp
@@ -1,4 +1,73 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Recepta;
 class Skierowanie;
-class Pacjent;
-class Recepcjonistka;
+
+class Osoba {
+	string imie;
+	string nazwisko;
+public:
+	string getImie() {
+		return imie;
+	}
+
+	void setImie(string i) {
+		imie = i;
+	}
+
+	string getNazwisko() {
+		return nazwisko;
+	}
+
+	void setImie(string n) {
+		nazwisko = n;
+	}
+};
+
+class Recepcjonistka : public Osoba {};
+
+class Lekarz : public Osoba {
+	string tytul;
+	string specjalizacja;
+public:
+	string getTytul() {
+		return tytul;
+	}
+
+	void setTytul(string t) {
+		tytul = t;
+	}
+
+	string getSpecjalizacja() {
+		return specjalizacja;
+	}
+
+	void setSpecjalizacja(string s) {
+		specjalizacja = s;
+	}
+};
+class dfd fdfdff;
+
+class Pacjent : public Osoba {
+	int rok_urodzenia;
+	string pesel;
+public:
+	string getPesel() {
+		return pesel;
+	}
+
+	void setPesel(string p) {
+		pesel = p;
+	}
+
+	int getRok_Urodzenia() {
+		return rok_urodzenia;
+	}
+
+	void setRok_Urodzenia(int ru) {
+		rok_urodzenia = ru;
+	}
+};
